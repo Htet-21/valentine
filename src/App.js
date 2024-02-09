016b8b37bf88f6c9989b59fcfdce780026ef0d97
Hein Htet <93510683+Htet-21@users.noreply.github.com>
2024-02-09 21:50:13 +0630

    Final App.js

--- src/App.js
+++ src/App.js
@@ -18,19 +18,19 @@ export default function App() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
+      "You might regret this!",
+      "This could be a mistake!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
       "Are you sure?",
       "Really sure?",
       "Think again!",
       "Last chance!",
       "Surely not?",
-      "You might regret this!",
       "Give it another thought!",
       "Are you absolutely certain?",
-      "This could be a mistake!",
       "Have a heart!",
       "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
       "Is that your final answer?",
       "You're breaking my heart ;(",
     ];
@@ -42,12 +42,13 @@ export default function App() {
     <div className="flex flex-col items-center justify-center h-screen -mt-16">
     {yesPressed ? (
       <>
-      <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-      <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+      <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExb2RtcjNlNnViM2hxOWhkaThyOWl6dGN1NXJwdHo0NWI3czByeWZwdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/cVdJgRyXv2j5meylHX/giphy.gif" />
+      <div className="text-4xl font-bold my-4">Now Let's Dateee</div>
       </>
     ) : (
         <>
-          <img className="h-[200px]" src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" />
+          <img className="h-[200px]" src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDVhZGsyNGhtZ3Q4bmxwa2RwN3ozM2ljdTQ1ZzhzMXlremd3YWxvdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/k2aHOBybkSSlqWc5XD/giphy.gif" />
+          <h1 className="text-4sm my-4">↑ Hein Htet Soe Moe ↑</h1>
           <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
           <div>
             <button
